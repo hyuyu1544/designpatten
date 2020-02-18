f85bef499a82ad3e6962c873489f983a881f346c
Yu Yu Huang <hyuyu1544@gmail.com>
2020-02-18 18:11:40 +0800

    fix: dev: modify state base

--- state/state.py
+++ state/state.py
@@ -1,17 +1,39 @@
 """State Pattern."""
+from abc import ABCMeta, staticmethod
 
 
-class context:
-    pass
+class Context(metaclass=ABCMeta):
+    def __init__(self):
+        pass
+
+    def add_state(self):
+        pass
+
+    def change_state(self):
+        pass
+
+    def get_state(self):
+        pass
+
+    def _set_state_info(self):
+        pass
+
+    def _get_state_info(self):
+        pass
 
 
-class state:
-    pass
+class State:
+    """state base."""
 
+    def __init__(self, name):
+        self.__name = name
 
-class stateA(state):
-    pass
+    def get_name(self):
+        return self.__name
 
+    def is_match(self, state_info):
+        pass
 
-class stateB(state):
-    pass
+    @staticmethod
+    def behavior(self):
+        pass

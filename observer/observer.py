f4c5be6bb821ff0cd0cf6ddae92e34bfb9c5aa82
Yu-Yu Huang <52728134+hyuyu1544@users.noreply.github.com>
2020-02-24 21:50:53 +0800

    fix: dev: modify

--- observer/observer.py
+++ observer/observer.py
@@ -23,14 +23,3 @@ class Observable:
     def notify_observer(self, message='TEST'):
         for obj in self.observers:
             obj.update(self, message)
-
-
-if __name__ == "__main__":
-
-    school_timetable = Target()
-    English_Teacher = EnglishTeacher()
-    Math_Teacher = MathTeacher()
-    school_timetable.add_observer(English_Teacher)
-    school_timetable.add_observer(Math_Teacher)
-    school_timetable.set_message('english class')
-    school_timetable.set_message('math class')
